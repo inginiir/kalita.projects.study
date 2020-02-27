c4a90fe2a0885a08eab7e0ec008b17a34fd2e389
inginiir <45371060+inginiir@users.noreply.github.com>
2020-02-27 22:02:32 +0500

    try

---
+++ src/main/java/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        User user = new User("Pavel");
+        User user2 = new User("Elena");
+        User user3 = new User("Elena");
+    }
+
+}
