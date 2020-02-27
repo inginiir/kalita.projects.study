c4b192929009d79aa7d4026a6ce6e541e9c6e82c
inginiir <45371060+inginiir@users.noreply.github.com>
2020-02-27 22:53:18 +0500

    try

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -5,6 +5,7 @@ public class Test {
         User user = new User("Pavel");
         User user2 = new User("Elena");
         User user3 = new User("Elena");
+        User user5 = new User("Elena");
     }
 
 }
