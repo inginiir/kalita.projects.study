49446bb6e5abfeb10cfc60e829405d6d0bf697d3
inginiir <pkalita@mail.ru>
2020-03-17 16:13:20 +0500

    refresh

---
+++ src/main/java/servlets/AllRequestsServlet.java
@@ -0,0 +1,49 @@
+package servlets;
+
+import templater.PageGenerator;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class AllRequestsServlet extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, Object> pageVariables = createPageVariableMap(request);
+        pageVariables.put("message", "");
+
+        response.getWriter().println(PageGenerator.instance().getPage("mirror/page.html", pageVariables));
+
+        response.setContentType("text/html; charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, Object> pageVariables = createPageVariableMap(request);
+
+        String message = request.getParameter("message");
+        response.setContentType("text/html; charset=utf-8");
+
+        if (message == null || message.isEmpty()) {
+            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        } else response.setStatus(HttpServletResponse.SC_OK);
+
+        pageVariables.put("message", message == null ? "" : message);
+
+        response.getWriter().println(PageGenerator.instance().getPage("mirror/page.html", pageVariables));
+    }
+
+    private Map<String, Object> createPageVariableMap(HttpServletRequest request) {
+        Map<String, Object> pageVariables = new HashMap<>();
+        pageVariables.put("method", request.getMethod());
+        pageVariables.put("URL", request.getRequestURL().toString());
+        pageVariables.put("pathInfo", request.getPathInfo());
+        pageVariables.put("sessionID", request.getSession().getId());
+        pageVariables.put("parameters", request.getParameterMap().toString());
+        return pageVariables;
+    }
+}
